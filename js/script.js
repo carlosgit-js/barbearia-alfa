4278e2b41fc822c058715da75e8c4ea8d655d5a6
Carlos e familia <carloshnr.dev@gmail.com>
2026-03-08 17:18:46 -0300

    Feat: section .hero concluida

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const hamburger = document.getElementById('hamburger')
 const menu = document.getElementById('menu')
 const header = document.getElementById('header')
 
-
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
     menu.classList.toggle('active')
